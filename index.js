2adc9d3129c319b44ae793de4de251ab38a16794
Victoria <victoria.eisemann@gmail.com>
2020-08-06 07:05:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ function sayHiToGrandma(string) {
   if(string==="I love you, Grandma."){
     return("I love you, too.");
   }
-  else if(){
+  else if(string===string.toLowerCase()){
     return("I can't hear you!");
   }
-  else if(){
+  else if(string===string.toUpperCase()){
     return("YES INDEED!");
   }
 }
